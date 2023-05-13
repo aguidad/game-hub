6bee955517aff99ab80459ea72afc0ec9d855e4f
guidev <aimenguidad@gmail.com>
2023-05-13 15:22:34 +0100

    add switch mode

---
+++ src/components/ColorModeSwitch.tsx
@@ -0,0 +1,23 @@
+import { Button, HStack, IconButton, useColorMode } from "@chakra-ui/react";
+import { MdLightMode, MdDarkMode } from "react-icons/md";
+
+const ColorModeSwitch = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+  const isLight = colorMode === "light";
+
+  return (
+    <HStack>
+      <IconButton
+        border={0}
+        fontSize="25px"
+        variant="outline"
+        aria-label="sw mode"
+        colorScheme={isLight ? "blackAlpha" : "yellow"}
+        icon={isLight ? <MdDarkMode /> : <MdLightMode />}
+        onClick={toggleColorMode}
+      />
+    </HStack>
+  );
+};
+
+export default ColorModeSwitch;
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,14 @@
+import { HStack, Image, Text } from "@chakra-ui/react";
+import logo from "assets/logo.webp";
+import ColorModeSwitch from "./ColorModeSwitch";
+
+const NavBar = () => {
+  return (
+    <HStack padding="10px" justifyContent="space-between">
+      <Image src={logo} boxSize="60px" />
+      <ColorModeSwitch />
+    </HStack>
+  );
+};
+
+export default NavBar;
