45472e98d53cdeb212b146198b0a5df721428d24
guidev <aimenguidad@gmail.com>
2023-05-17 23:35:09 +0100

    remove unused import

--- src/theme.ts
+++ src/theme.ts
@@ -1,8 +1,4 @@
-import {
-  extendTheme,
-  withDefaultColorScheme,
-  type ThemeConfig,
-} from "@chakra-ui/react";
+import { extendTheme, type ThemeConfig } from "@chakra-ui/react";
 
 const config: ThemeConfig = {
   initialColorMode: "dark",
